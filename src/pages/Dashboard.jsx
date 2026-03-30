ab700975f9c60dc59f57501be421f73d480e31f1
Vijay_dev_2026 <vijaybhivagade14@gmail.com>
2026-03-30 10:06:06 +0530

    added icons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,35 +3,38 @@ import MainLayout from "../layouts/MainLayout";
 export default function Dashboard() {
   return (
     <MainLayout title="Dashboard">
-      <div className="cards">
+
+      <div className="dashboard-grid">
+
         <div className="card">
-          <h4>Total Patients</h4>
+          <h3>Total Patients</h3>
           <p>1</p>
         </div>
 
         <div className="card">
-          <h4>Total Tests</h4>
+          <h3>Total Tests</h3>
           <p>2</p>
         </div>
 
         <div className="card">
-          <h4>Reports Generated</h4>
+          <h3>Reports Generated</h3>
           <p>0</p>
         </div>
 
         <div className="card">
-          <h4>Tests Today</h4>
+          <h3>Tests Today</h3>
           <p>2</p>
         </div>
+
       </div>
 
-      <h3>Quick Actions</h3>
-
-      <div className="cards">
-        <div className="card">Add Patient</div>
-        <div className="card">Add Test</div>
-        <div className="card">Generate Report</div>
+      <div className="quick-actions">
+        <h3>Quick Actions</h3>
+        <button>Add Patient</button>
+        <button>Add Test</button>
+        <button>Generate Report</button>
       </div>
+
     </MainLayout>
   );
 }
