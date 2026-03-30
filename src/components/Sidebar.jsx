1fa0dcb2f0bd42e5a3cf77395c0bc364a23ae9ed
Vijay_dev_2026 <vijaybhivagade14@gmail.com>
2026-03-30 10:04:31 +0530

    linked sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,12 +3,17 @@ import { NavLink } from "react-router-dom";
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <h2 className="logo">METACORE</h2>
+
+      {/* ONLY LOGO */}
+      <div className="sidebar-logo">
+        <img src="/metacore.png" alt="logo" />
+      </div>
 
       <NavLink to="/" end>Dashboard</NavLink>
       <NavLink to="/patients">Patients</NavLink>
       <NavLink to="/tests">Tests</NavLink>
       <NavLink to="/reports">Reports</NavLink>
+
     </div>
   );
 }
